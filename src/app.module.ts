40958f4de22fa8017f0b35d62cde37442b2e1e6e
AlexandrKhom <alex988andr@gmail.com>
2021-11-25 00:54:16 +0200

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [__dirname + '/shared/db/entities/**/*.entity.{js,ts}'],
+      migrationsTableName: 'migration',
+      migrations: [__dirname + '/migrations/*.js'],
+      cli: {
+        migrationsDir: 'src/migrations',
+      },
+      synchronize: false,
+    }),
+
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
